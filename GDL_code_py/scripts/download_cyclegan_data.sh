fd91e143fc4a6380f30b1dc34f9d7b266fdec356
idfree <151384146+idfree@users.noreply.github.com>
2024-01-17 12:34:32 -0800

    update the link

--- GDL_code_py/scripts/download_cyclegan_data.sh
+++ GDL_code_py/scripts/download_cyclegan_data.sh
@@ -6,7 +6,7 @@ if [[ $FILE != "ae_photos" && $FILE != "apple2orange" && $FILE != "summer2winter
     exit 1
 fi
 
-URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
+URL=https://efrosgans.eecs.berkeley.edu/cyclegan/datasets/$FILE.zip
 ZIP_FILE=./data/$FILE.zip
 TARGET_DIR=./data/$FILE/
 wget -N $URL -O $ZIP_FILE
